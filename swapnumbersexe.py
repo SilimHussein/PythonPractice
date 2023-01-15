5ab3f973277077965d5a1be28644947e731e4d32
SilimHussein <silimhussein16@gmail.com>
2023-01-15 09:30:35 +0300

    a few annotations to make the code clean

--- swapnumbersexe.py
+++ swapnumbersexe.py
@@ -1,10 +1,10 @@
 a = input("enter value of a ")
 b = input("enter value of b ")
-print (a)
-print (b)
+print ("a = ", a)
+print ("b = ", b)
 c = a
 a = b
 b = c
 print("the reversed numbers are ")
-print (a)
-print (b)
+print ("a = ", a)
+print ("b = ", b)
