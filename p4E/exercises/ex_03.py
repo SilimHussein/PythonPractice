a6f63a57451611b1611b0b4ef18b0c3127c968b6
SilimHussein <silimhussein16@gmail.com>
2023-06-30 17:57:06 +0300

    ex 3 and 4 were not commited

---
+++ p4E/exercises/ex_03.py
@@ -0,0 +1,4 @@
+hrs = input("Enter Hours: ")
+rate = input("Enter rate: ")
+gpay= float(hrs) * float(rate)
+print("gross pay = ", gpay)
---
+++ p4E/exercises/ex_04.py
@@ -0,0 +1,3 @@
+celcius = input("Enter Temp in Celcius : ")
+farhenheit = (int(celcius) * 9/5) + 32
+print ("Temperature in Fahrenheit = : ", farhenheit)
