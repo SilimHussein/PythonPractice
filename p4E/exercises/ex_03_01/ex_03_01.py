2977fb39efaccf45f7436950eaee928afe0a00df
SilimHussein <silimhussein16@gmail.com>
2023-06-30 07:00:30 +0300

    exercise 3.1 finished

--- p4E/exercises/ex_03_01/ex_03_01.py
+++ p4E/exercises/ex_03_01/ex_03_01.py
@@ -3,6 +3,16 @@ sh = input("Enter hours:")
 sr = input("Enter rate:")
 fh = float(sh)
 fr = float(sr)
-#over time
+#overtime
 if fh > 40:
-    
+    reg = fh * fr
+    otp = (fh - 40) * (fr * 0.5)
+    #rint(reg, otp)
+    #print("overtime")
+    gp = reg + otp
+    print ("gross pay :", gp)
+else:
+#regular time
+    #print("regular time")
+    gp = fh * fr
+    print("gross pay", gp)
