f9987f922eb4eea0838598ddbb3ec62459ba95c9
SilimHussein <silimhussein16@gmail.com>
2023-06-30 17:55:17 +0300

    exercise 3.3 completed

--- p4E/exercises/ex_03_01/ex_03_02.py
+++ p4E/exercises/ex_03_01/ex_03_02.py
@@ -1,7 +1,16 @@
 #user input
 score = input("Enter a score :")
 fscore = float(score)
-if fscore >= 0.0 and fscore <= 0.1:
-    print(fscore)
+if fscore >= 0.0 and fscore <= 1.0:
+    if fscore >= 0.9:
+        print("A")
+    elif fscore >= 0.8:
+        print("B")
+    elif fscore >= 0.7:
+        print("C")
+    elif fscore >= 0.6:
+        print("D")
+    elif fscore < 0.6:
+        print("E")
 else:
     print("out of range")
