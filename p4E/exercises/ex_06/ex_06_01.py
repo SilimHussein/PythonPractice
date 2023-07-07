30eed7a2570b77d80257eb311f8bd70efe0c70be
SilimHussein <silimhussein16@gmail.com>
2023-07-07 18:28:18 +0300

    traversing through string characters with loops

---
+++ p4E/exercises/ex_06/ex_06_01.py
@@ -0,0 +1,7 @@
+fruit = 'banana'
+index = 0
+#gives the range of traversal, from 0 - the entire length of the string
+while index < len(fruit):
+    letter = len[index-1]
+    print(letter)
+    index = index - 1
