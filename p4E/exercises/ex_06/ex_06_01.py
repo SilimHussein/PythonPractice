e6a4efb593272c391952e83962e7f213976361d1
SilimHussein <silimhussein16@gmail.com>
2023-07-08 05:40:54 +0300

    exercise 06_01 rectified

--- p4E/exercises/ex_06/ex_06_01.py
+++ p4E/exercises/ex_06/ex_06_01.py
@@ -1,7 +1,7 @@
 fruit = 'banana'
-index = 0
+index = len(fruit)
 #gives the range of traversal, from 0 - the entire length of the string
-while index < len(fruit):
-    letter = len[index-1]
+while index > 0:
+    letter = fruit[index-1]
     print(letter)
     index = index - 1
