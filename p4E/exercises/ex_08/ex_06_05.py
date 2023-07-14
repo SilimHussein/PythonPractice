692394ca4136db4b5597659f92b7c11f3a17184d
SilimHussein <silimhussein16@gmail.com>
2023-07-14 05:55:26 +0300

    ex_06_05, parsing and splitting to find emails starting with from

---
+++ p4E/exercises/ex_08/ex_06_05.py
@@ -0,0 +1,16 @@
+fname = input('Enter the name of the file: ')
+count = 0
+try:
+    fhand = open(fname)
+except:
+    print('No such file')
+
+for line in fhand:
+    line = line.rstrip()
+    if not line.startswith('From '):
+        continue
+    words = line.split()
+    print(words[1])
+    count = count + 1
+
+print('There were',count, 'lines in the file with From as the first word')
