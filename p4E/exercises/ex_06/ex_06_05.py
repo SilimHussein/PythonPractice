1dd2d056b79344e13c56be81f8fc46bea0338f04
SilimHussein <silimhussein16@gmail.com>
2023-07-17 02:16:37 +0300

    reviews

--- p4E/exercises/ex_06/ex_06_05.py
+++ p4E/exercises/ex_06/ex_06_05.py
@@ -1,6 +1,8 @@
 str = 'X-DSPAM-Confidence:0.8475'
+#we are looking for our starting position which is :
 startpos = str.find(':')
 print(startpos)
+#we look for the first 5 after the starting position (startpos), which is our ending position(endpos)
 endpos = str.find("5",startpos)
 print(endpos)
 dspam = str[startpos+1:endpos+1]
