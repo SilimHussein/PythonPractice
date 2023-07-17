d406e29b3900cf3296bb7b0cba33fa81a28ddce8
SilimHussein <silimhussein16@gmail.com>
2023-07-17 03:45:43 +0300

    trying out string formatting during review

--- p4E/exercises/ex_07/userInputTosearch.py
+++ p4E/exercises/ex_07/userInputTosearch.py
@@ -9,5 +9,9 @@ count = 0
 for line in fhand:
     if line.startswith('Subject:'):
         count = count + 1
+#trying out the str.format() and str.format_map()
+#str.format()
+message = 'There were {} Subject lines in {}'.format(count, fname)
+print(message)
 
-print('There were', count, 'Subject lines in ', fname)
+#print('There were', count, 'Subject lines in ', fname)
