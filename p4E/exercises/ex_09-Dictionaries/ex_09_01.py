c3eb4ae8cef98602d0e1ef5124ec25999d2a7b3b
SilimHussein <silimhussein16@gmail.com>
2023-07-19 16:36:14 +0300

    exercise 09_01,reading words from a file and storing in a dictionary

---
+++ p4E/exercises/ex_09-Dictionaries/ex_09_01.py
@@ -0,0 +1,20 @@
+fname = input('Enter file name:')
+try:
+    fhand = open(fname)
+except:
+    print('File cannot be opened:', fname)
+    exit()
+
+counts = dict()
+#outer loop reads the lines of the file
+for line in fhand:
+    words = line.split()
+    #the inner loop iterates through each of the words on that particular line.
+    for word in words:
+        """if word not in counts:
+            counts[word] = 1
+        else:
+            counts[word] += 1
+        """
+        counts[word] = counts.get(word,0) + 1 
+print(counts)
