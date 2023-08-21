e4ea28bf73d9fbc18aedde9fb992e35f2b82c94b
SilimHussein <silihussein16@gmail.com>
2023-08-21 12:32:26 +0100

    consumer token and secret in hidden.py

--- p4E/exercises/ex_13-WebServices/twitter/hidden.py
+++ p4E/exercises/ex_13-WebServices/twitter/hidden.py
@@ -4,7 +4,7 @@
 # Create new App and get the four strings
 
 def oauth():
-    return {"consumer_key": "h7Lu...Ng",
-            "consumer_secret": "dNKenAC3New...mmn7Q",
-            "token_key": "10185562-eibxCp9n2...P4GEQQOSGI",
-            "token_secret": "H0ycCFemmC4wyf1...qoIpBo"}
+    return {"consumer_key": "EuRXkQNAG1Rrmv3a8MmY4nio6",
+            "consumer_secret": "HPTk4lPyWzH6NF2buhVyiEffbJD75j54tKVvpf7vLo2DQi8OoV",
+            "token_key": "791207222-WkeeGflUmHCnQS6ZcDrZYy6oNQIO6LRtvj0sa7pp",
+            "token_secret": "klLQTzR4MH5P38zdk37ZlJPhXNj0pxAY42um5249cct3h"}
