22557b59e5f43f8b2b90260456d6dcd9af2fa76d
SilimHussein <silihussein16@gmail.com>
2023-08-23 13:32:57 +0100

    twitter hidden file

--- p4E/exercises/ex_13-WebServices/twitter/hidden.py
+++ p4E/exercises/ex_13-WebServices/twitter/hidden.py
@@ -4,7 +4,7 @@
 # Create new App and get the four strings
 
 def oauth():
-    return {"consumer_key": "EuRXkQNAG1Rrmv3a8MmY4nio6",
-            "consumer_secret": "HPTk4lPyWzH6NF2buhVyiEffbJD75j54tKVvpf7vLo2DQi8OoV",
-            "token_key": "791207222-WkeeGflUmHCnQS6ZcDrZYy6oNQIO6LRtvj0sa7pp",
-            "token_secret": "klLQTzR4MH5P38zdk37ZlJPhXNj0pxAY42um5249cct3h"}
+    return {"consumer_key": "PJ7X78H9JYcEmLLKIfFHzTocx",
+            "consumer_secret": "hieUia6A7wpcs8jhWQiSRzJxXyXpVxPjYZhxku1Z1cDlq3Apgj",
+            "token_key": "791207222-yR5tacwEBMAYwzfJ5wEQi5cM5eQtaqrqkGdoarGo",
+            "token_secret": "xMu6csAOlqDyDjr8EtrqtM7r8AQBq5eIisDeQupCpGeEW"}
