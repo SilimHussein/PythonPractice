07b67bfda235899332e2d0915bbb48ab63c93392
SilimHussein <silihussein16@gmail.com>
2023-08-24 09:21:29 +0100

    json autograder2.py

--- p4E/exercises/ex_13-WebServices/autograder2.py
+++ p4E/exercises/ex_13-WebServices/autograder2.py
@@ -1,5 +1,6 @@
 import urllib.request, urllib.parse, urllib.error
-import ssl, json
+import ssl
+import json 
 
 #ignoring Certificate errors
 ctx = ssl.create_default_context()
@@ -7,11 +8,11 @@ ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
 #user request for URL
-url = input('Enter url:')
+url = input('Enter location:')
 
 #default URL testing
 if len(url) < 1:
-    url = "http://py4e-data.dr-chuck.net/comments_42.json"
+    url = "http://py4e-data.dr-chuck.net/comments_1737623.json"
 
 #print retrieving message
 print('Retrieving', url)
@@ -25,8 +26,21 @@ data = uh.read()
 #printing to match format text
 print('Retrieved', len(data), 'characters')
 
-info = json.loads(data)
-print('Count:', len(info))
+#error checking, loading js
+try:
+    info = json.loads(data)
+except:
+    info = None
+
+#calculating and printing count and sum
+count = 0
+tot = 0
+for item in info['comments']:
+    count = count + 1
+    tot = int(item['count']) + tot
+
+print('Count', count)
+print('Sum', tot)
 
 
 
