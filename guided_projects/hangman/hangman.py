75b398ccd30ebef59ea6a7e19ce87af6515397fb
Silim Hussein <silimhussein16@gmail.com>
2025-03-27 06:38:11 +0300

    added lives in hangman, and comment on list comprehension

--- guided_projects/hangman/hangman.py
+++ guided_projects/hangman/hangman.py
@@ -14,13 +14,31 @@ def hangman():
     word_letters = set(word) #letters in the word
     alphabet = set(string.ascii_uppercase)
     used_letters = set() # what the user has guessed
+    lives = 6
 
     # getting user input
-    while len(word_letters) > 0:
+    while len(word_letters) > 0 and lives > 0:
         # letters used
-        print("You have used these letters: ", " ".join(sorted(used_letters)))
+        print(f"you have {lives} left and you have used these letters: ", " ".join(sorted(used_letters)))
 
         # what current word is (i.e W-RD)
+        '''
+        word_list = []  # Create an empty list
+        for letter in word:  
+            if letter in used_letters:  
+                word_list.append(letter)  # Keep the letter if guessed
+            else:
+                word_list.append('-')  # Hide the letter if not guessed
+    
+        LIST COMPREHENSION
+        [expression for item in collection if condition]
+        Expression → What to do with each item.
+
+        Collection → The list (or range) we're looping through.
+
+        Condition (Optional) → A filter to pick only some items.
+
+        '''
         word_list = [letter if letter in used_letters else '-' for letter in word]
         print('Current word: ', ' '.join(word_list))
 
@@ -29,15 +47,22 @@ def hangman():
             used_letters.add(user_letter)
             if user_letter in word_letters:
                 word_letters.remove(user_letter)
+            
+            else:
+                lives -= 1
+                print('Letter is not in the word')
         
         elif user_letter in used_letters:
             print("You have already used that character. Please try again. ")
 
         else:
             print("Invalid character. Please try again.")
-        
-    print(f"Congratulations! you have guessed the word : {word} correctly!")
     
-    #gets here when len(word_letters) == 0
+    if lives == 0:
+        print(f'You died. The word was {word}')
+    else: 
+        print(f"Congratulations! you have guessed the word : {word} correctly!")
+    
+    #gets here when len(word_letters) == 0 and lives == 0
 
 hangman()
